2e790316429a17d185fcfcd7909d1830ba6dc20f
Sarfaraz45 <56291130+Sarfaraz45@users.noreply.github.com>
2022-02-08 18:54:24 +0500

    Update script.js
    added event listener

---
+++ P-01/script.js
@@ -0,0 +1,26 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const password2 = document.getElementById('password2')
+
+
+
+
+// All functions
+// Functions to show error
+function showError(input,message){
+ const formControl = input.parentElement
+ formControl.className = 'form-control error'
+ //const small = formControl.querySelector('small')
+}
+
+
+
+//THis is an event listener for the form on submit
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+    if (username.value === '') {
+        showError(username,'username is required')
+    }
+})
