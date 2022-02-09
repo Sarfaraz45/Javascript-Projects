c600b34fe568d981047ab17b103f491f343221d7
Sarfaraz45 <56291130+Sarfaraz45@users.noreply.github.com>
2022-02-09 10:47:39 +0500

    Update script.js
    Created JS code for requird check using simple JS

--- P-01/script.js
+++ P-01/script.js
@@ -12,9 +12,16 @@ const password2 = document.getElementById('password2')
 function showError(input,message){
  const formControl = input.parentElement
  formControl.className = 'form-control error'
- //const small = formControl.querySelector('small')
+ const small = formControl.querySelector('small')
+ small.innerText = message
 }
 
+//Function to show success
+
+function showSuccess(input){
+    const formControl = input.parentElement
+    formControl.className = 'form-control success'
+}
 
 
 //THis is an event listener for the form on submit
@@ -22,5 +29,24 @@ form.addEventListener('submit', function(e){
     e.preventDefault()
     if (username.value === '') {
         showError(username,'username is required')
+    } else{
+        showSuccess(username)
+    }
+    if (email.value === '') {
+        showError(email,'Email is required')
+    } else{
+        showSuccess(email)
+    }
+
+    if (password.value === '') {
+        showError(password,'Password is required')
+    } else{
+        showSuccess(password)
+    }
+
+    if (password2.value === '') {
+        showError(password2,'Confirm Password is required')
+    } else{
+        showSuccess(password2)
     }
 })
