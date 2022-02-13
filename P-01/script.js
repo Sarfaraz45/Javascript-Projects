d71abdb4000f3a7b90a9d772fe78ca6eac8894c1
Sarfaraz45 <56291130+Sarfaraz45@users.noreply.github.com>
2022-02-13 13:43:11 +0500

    archive script 1 and create new script.js
    Refactored required field check

--- P-01/script.js
+++ P-01/script.js
@@ -30,33 +30,26 @@ function showSuccess(input){
     formControl.className = 'form-control success'
 }
 
+//Function to check if required fields have data
+function checkRequired(inputArray){
+inputArray.forEach(function(input)  {
+    if(input.value === ''){
+        console.log(input.id)
+        showError(input, `${getfieldId(input)} is required`)
+    } else{
+        showSuccess(input)
+    }
+});
+}
 
+//Function to get the id of the input feild
+function getfieldId(input){
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
 //THis is an event listener for the form on submit
 form.addEventListener('submit', function(e){
     e.preventDefault()
-    if (username.value === '') {
-        showError(username,'username is required')
-    } else{
-        showSuccess(username)
-    }
-    if (email.value === '') {
-        showError(email,'Email is required')
-    } else if (!isValidEmail(email.value) ) {
-        showError(email,"Email is Invalid")
-    } 
-    else{
-        showSuccess(email)
-    }
 
-    if (password.value === '') {
-        showError(password,'Password is required')
-    } else{
-        showSuccess(password)
-    }
-
-    if (password2.value === '') {
-        showError(password2,'Confirm Password is required')
-    } else{
-        showSuccess(password2)
-    }
-})
+    checkRequired([username,email,password,password2])
+    
+  })
