d25dc74487a793ac1ee111ecf3d2f018edb5d9b7
Sarfaraz45 <56291130+Sarfaraz45@users.noreply.github.com>
2022-02-13 16:16:29 +0500

    updated
    added functionality to check email validation check length validation and password verification check

--- P-01/script.js
+++ P-01/script.js
@@ -18,9 +18,13 @@ function showError(input,message){
 
 //Function to check if email is valid
 
-function isValidEmail(email){
+function checkEmail(input){
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if(re.test(input.value.trim())){
+        showSuccess(input)
+    } else{
+        showError(input, `Please provide a valid email`)
+    }
 }
 
 //Function to show success
@@ -41,6 +45,23 @@ inputArray.forEach(function(input)  {
     }
 });
 }
+// Function to check if password and confirm password match
+function checkPasswordsMatch(input1, input2){
+    if(input1.value !== input2.value){
+        showError(input2,"Passwords don't match")
+    }
+}
+
+//Function to check length of input field
+function checkLength(input, min, max){
+    if(input.value.length < min){
+        showError(input, `${getfieldId(input)} needs to be at least ${min} characters`)
+    } else if(input.value.length > max){
+        showError(input, `${getfieldId(input)} needs to be at least ${max} characters`)
+    } else{
+        showSuccess(input)
+    }
+}
 
 //Function to get the id of the input feild
 function getfieldId(input){
@@ -51,5 +72,9 @@ form.addEventListener('submit', function(e){
     e.preventDefault()
 
     checkRequired([username,email,password,password2])
+    checkLength(username,3,10)
+    checkLength(password,6,30)
+    checkEmail(email)
+    checkPasswordsMatch(password,password2)
     
   })
